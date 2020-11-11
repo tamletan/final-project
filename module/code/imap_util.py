2fe3c5264219ae6c1377494d8e8aa048ca75599e
Tấn Tâm Lê <tamletannk94@gmail.com>
2020-11-11 13:53:36 +0700

    modify imap_util

--- module/code/imap_util.py
+++ module/code/imap_util.py
@@ -12,10 +12,6 @@ from bs4 import BeautifulSoup
 import filter_en as fte
 import utility as utl
 
-def get_auth(cred):
-	df = pd.read_csv(cred)
-	return df['usr'][0], df['pwd'][0]
-
 def parser():
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-c", "--cred", type=str, default=r'..\data\credentials.csv',
@@ -49,7 +45,7 @@ def read_imap(cred):
 def open_imap(cred):
 	# create an IMAP4 class with SSL 
 	imap = imaplib.IMAP4_SSL("imap.gmail.com", '993')
-	username, password = get_auth(cred)
+	username, password = utl.get_auth(cred)
 	# authenticate
 	imap.login(username, password)
 	return imap
